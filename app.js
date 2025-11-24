695d71fab5fd1bfeccd21449890ffb317587b5c9
Birdavinder03 <birdavindersinghchahal@gmail.com>
2025-11-24 17:16:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById("checkBtn").addEventListener("click", function() {
+    let amount = document.getElementById("amountInput").value;
+
+    console.log("Amount entered: " + amount);
+
+    alert("You entered: " + amount);
+});
